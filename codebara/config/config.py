9ca7c8a0699fa0fdad493360ce00f50ebeeeb1a1
champix56 <desorbaix@free.fr>
2026-02-22 09:56:08 +0100

    config as package

--- codebara/config/config.py
+++ codebara/config/config.py
@@ -4,14 +4,14 @@ IMAGE_IA_GEN_ENDPOINT_METHOD="POST"
 IMAGE_IA_GEN_BODY_BASE={
   "lcm_model_id": "stabilityai/sd-turbo",
   "openvino_lcm_model_id": "rupeshs/sd-turbo-openvino",
-  "use_offline_model": false,
-  "use_lcm_lora": false,
+  "use_offline_model": False,
+  "use_lcm_lora": False,
   "lcm_lora": {
     "base_model_id": "Lykon/dreamshaper-8",
     "lcm_lora_id": "latent-consistency/lcm-lora-sdv1-5"
   },
-  "use_tiny_auto_encoder": false,
-  "use_openvino": false,
+  "use_tiny_auto_encoder": False,
+  "use_openvino": False,
   "prompt": "",
   "negative_prompt": "",
   "init_image": "string",
@@ -24,27 +24,27 @@ IMAGE_IA_GEN_BODY_BASE={
   "token_merging": 0,
   "number_of_images": 1,
   "seed": 123123,
-  "use_seed": false,
-  "use_safety_checker": false,
+  "use_seed": False,
+  "use_safety_checker": False,
   "diffusion_task": "text_to_image",
   "lora": {
     "models_dir": "/home/champix/PycharmProjects/fastsdcpu/lora_models",
     "weight": 0.5,
-    "fuse": true,
-    "enabled": false
+    "fuse": True,
+    "enabled": False
   },
   "controlnet": {
     "adapter_path": "string",
     "conditioning_scale": 0.5,
-    "enabled": false
+    "enabled": False
   },
   "dirs": {
     "controlnet": "/home/champix/PycharmProjects/fastsdcpu/controlnet_models",
     "lora": "/home/champix/PycharmProjects/fastsdcpu/lora_models"
   },
-  "rebuild_pipeline": false,
-  "rebuild_controlnet_pipeline": false,
-  "use_gguf_model": false,
+  "rebuild_pipeline": False,
+  "rebuild_controlnet_pipeline": False,
+  "use_gguf_model": False,
   "gguf_model": {
     "gguf_models": "/home/champix/PycharmProjects/fastsdcpu/models/gguf"
   }
