d02984ab82c5d05e60688d0339a205ca411f4787
champix56 <desorbaix@free.fr>
2026-02-22 09:56:43 +0100

    fix order

--- codebara/cards/types.py
+++ codebara/cards/types.py
@@ -1,11 +1,22 @@
+import random
+class CardSpeciality:
+    Attack:int|None
+    Healt:int|None
+    name:str
+class CardSpec:
+    health:int
+    primaryAttack:int
+    secondariesSpecs: tuple[CardSpeciality]
+
 class CardDatas:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self,id:int|None,creatorId):
+        self.id = id if id is not None else random.randint(0,100000000000000)
+        self.creatorId=creatorId
     id:int
     creatorId:int
     name:str|None
     imageBlob:str|None
-    imageUrl|string|None
+    imageUrl:str|None
     specs:CardSpec
 
     #seasonMetas
@@ -15,18 +26,9 @@ class CardDatas:
     temporaryId:int|None
     genDate:str
     genTime:float
-    startDateTime:str
+    startDateTime:datetime
     cb:str
 
     #controlDatas
     checksum:str
     currentSign:str
-
-class CardSpec:
-    health:int
-    primaryAttack:int
-    secondariesSpecs: tuple[CardSpeciality]
-class CardSpeciality:
-    Attack:int|None
-    Healt:int|None
-    name:str
