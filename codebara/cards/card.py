81c52a478071a5223e99b5db81fabe3d081b0341
champix56 <desorbaix@free.fr>
2026-02-22 09:56:30 +0100

    fix card for threading

--- codebara/cards/card.py
+++ codebara/cards/card.py
@@ -1,18 +1,23 @@
 from .types import CardDatas
 import datetime
+import threading
+import codebara
 class Card(CardDatas):
     temporaryResponse=None
+    def __init__(self):
+        super.__init__(id=None,creatorId=0)
     @staticmethod
-    def gen(cb:str):
+    def gen(cb:str,uid:int=0):
         card=Card()
         card.cb=cb
         card.startDateTime = datetime.datetime.now()
          # Création de 2 threads
-        thread = threading.Thread(card._internalGen) 
+        card._threadGen()
         return card
-    
+    def _threadGen(self):
+        return threading.Thread(target=self._internalGen)
     def _internalGen(self):
-        response = requests.post(url, json=data)
+        response = requests.post(codebara.config.IMAGE_IA_GEN_REST_URL,json=codebara.config.IMAGE_IA_GEN_BODY_BASE)
         # Print the response
         self.temporaryResponse=response.json()
         print(self.temporaryResponse)  
