f9a180975a7f6b64d7abf669f4f955475fc01f8e
champix56 <desorbaix@free.fr>
2026-03-02 14:29:36 +0100

    add construc on card specs and specials specs class

--- codebara/cards/card.py
+++ codebara/cards/card.py
@@ -3,15 +3,20 @@ class SpecialSpec:
     attack:int=0
     health:int=0
     turn:int=-1
+    def __init__(self, name, attack:int=0,health:int=0, turn:int=-1):
+        self.name = name
+        self.attack=attack
+        self.health=health
+        self.turn=turn
 
 class CardSpecs:
     name:str
     attack:int
     health:int
-    specs:list[SpecialSpec]|None
+    specs:list[SpecialSpec]
     def __init__(self,name:str="",attack:int=0,
-                health:int=0,specs=None):
+                health:int=0,specs:list[SpecialSpec]|None=None):
         self.name=name
         self.attack=attack
         self.health=health
-        self.specs=specs
+        self.specs=specs if specs is not None else []
