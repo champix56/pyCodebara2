ab17663b80ad2b6296b646bf97edb27abecfc293
champix56 <desorbaix@free.fr>
2026-03-02 09:09:21 +0100

    fix awaitable

--- codebara/routes/routes.py
+++ codebara/routes/routes.py
@@ -1,14 +1,13 @@
 from aiohttp import web
 from ..errors import assembleHttpRequestError, HttpErrors
-from codebara.cards import card_route
+from codebara.cards import  cardBodyRoute
 async def noBodyRoute(request:web.Request,queryArray:tuple)->web.Response:
     return web.Response()
 async def bodyRoute(request:web.Request,body:dict,queryArray:tuple)->web.Response:
     response=assembleHttpRequestError(error=HttpErrors.ERROR_INVALID_ROUTE, request=request).toResponse()
     if request.path.startswith('/card'):
-        response=card_route.bodyRoute(request=request,body=body,queryArray=queryArray)
+        response=await cardBodyRoute(request=request,body=body,queryArray=queryArray)
     return response
-
 """async def get(request:web.Request,queryArray)->HttpOutputResponse:
     return HttpOutputResponse()
 async def post(request:web.Request,body,queryArray)->HttpOutputResponse:
