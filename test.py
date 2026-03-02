be2298c74c73499dd2ad4040fe80ab805f342a64
champix56 <desorbaix@free.fr>
2026-03-02 14:28:54 +0100

    test gen  card

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 from codebara.cards.imageCardCreator import CardImageCreator
-card = CardImageCreator()
-card.create("tuioiuythdfgiuopouy")
+import json
+from codebara.cards import CardSpecs, SpecialSpec
+sSpecs=[SpecialSpec(name="abc"),SpecialSpec(name="def"),]
+spec=CardSpecs("Mon premier Codebara", 89,65, sSpecs)
+data=None
+with open('./season/season.json', "r") as jsonfile:
+    data = json.load(jsonfile)
+    print(type(data))
+    print((data))
+card = CardImageCreator(uid=23432,season=data )
+
+card.create(specs=spec)
