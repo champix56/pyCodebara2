ea99f34e63fc63a1fd785dc981bcabd84597116d
champix56 <desorbaix@free.fr>
2026-03-02 14:30:03 +0100

    add user data and separator core user datas

--- codebara/user/user.py
+++ codebara/user/user.py
@@ -1,5 +1,11 @@
-class User:
-    def __init__(self, uid,seed):
+class UserCoreDatas:
+    def __init__(self, uid: int, seed: int):
         self.uid = uid
-        self.seed=seed
-    
+        self.seed = seed
+
+class User(UserCoreDatas):
+    def __init__(self, uid: int, seed: int, nickname: str, mail: str, amount: int):
+        super(uid, seed)
+        self.mail = mail
+        self.nickname = nickname
+        self.amount = amount
