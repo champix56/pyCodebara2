10a932fe3d8d253b19c88052c70bfc3d877e9ebd
champix56 <desorbaix@free.fr>
2026-03-02 15:04:53 +0100

    add zip compare test

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
-from codebara.cards.imageCardCreator import CardImageCreator
-import json
+""" image car gen force central image"""
+"""from codebara.cards.imageCardCreator import CardImageCreator
 from codebara.cards import CardSpecs, SpecialSpec
+from codebara.seasons.season import seasonLoader
 sSpecs=[SpecialSpec(name="abc"),SpecialSpec(name="def"),]
 spec=CardSpecs("Mon premier Codebara", 89,65, sSpecs)
-data=None
-with open('./season/season.json', "r") as jsonfile:
-    data = json.load(jsonfile)
-    print(type(data))
-    print((data))
-card = CardImageCreator(uid=23432,season=data )
+season=seasonLoader()
+card = CardImageCreator(uid=23432,season=season )
+locfile=card.create(specs=spec, centralImageLoc=None)"""
 
-card.create(specs=spec)
+""" ZIP Compare with one file
+import zipfile
+
+zip = zipfile.ZipFile("./stuffBZ2.zip", "w",compression= zipfile.ZIP_BZIP2, compresslevel=9)
+zip.write('./c.png')
+zip.close()
+zip = zipfile.ZipFile("./stuffLZMA.zip", "w",compression= zipfile.ZIP_LZMA, compresslevel=9)
+zip.write('./c.png')
+zip.close()
+zip = zipfile.ZipFile("./stuffDEF.zip", "w",compression= zipfile.ZIP_DEFLATED, compresslevel=9)
+zip.write('./c.png')
+zip.close()"""
