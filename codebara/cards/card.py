ecf3940ae17a20bc774a1ad081df5094f79c692e
champix56 <desorbaix@free.fr>
2026-03-20 02:15:04 +0100

    add check integrity

--- codebara/cards/card.py
+++ codebara/cards/card.py
@@ -1,3 +1,6 @@
+import json
+from io import TextIOWrapper
+from codebara.cards.cardMysql import check_cardHash
 class SpecialSpec:
     name:str
     attack:int=0
@@ -8,15 +11,62 @@ class SpecialSpec:
         self.attack=attack
         self.health=health
         self.turn=turn
-
+    def toJson(self)->str:
+        return json.dumps(self.__dict__)
 class CardSpecs:
+    id:int
     name:str
     attack:int
     health:int
-    specs:list[SpecialSpec]
-    def __init__(self,name:str="",attack:int=0,
-                health:int=0,specs:list[SpecialSpec]|None=None):
+    power:int=100
+    rarity:float=1.0
+    specials:list[SpecialSpec]
+    def __init__(self,cid:int,name:str="",attack:int=0,
+                health:int=0,specials:list[SpecialSpec]|None=None,power:int=100, rarity:float=1.0):
         self.name=name
         self.attack=attack
         self.health=health
-        self.specs=specs if specs is not None else []
+        self.rarity=rarity
+        self.power=power
+        self.specials=specials if specials is not None else []
+    def todict(self)->dict:
+        tempDictSpecs=[]
+        for spec in self.specials:
+            tempDictSpecs.append(spec.__dict__)
+        return {
+            "name":self.name,
+            "attack":self.attack,
+            "health":self.health,
+            "rarity":self.rarity,
+            "power":self.power,
+            "specials":tempDictSpecs }
+    def toJson(self)->str:
+        return json.dumps(self.todict())
+class Card (CardSpecs):
+    id:int
+    ownerid:int
+    creatorid:int
+    imageFileName:str
+    centralImageFileName:str
+    promptid:str
+    seasonid:int
+    seed:int
+    def __init__(self,id:int,seasonid:int,promptid:str,seed:int,ownerid:int,creatorid:int,name:str,imageFilename:str,centralImageFilename:str,attack:int=0,
+                health:int=0,specs:list[SpecialSpec]|None=None):
+        super().__init__(name,attack,health,specs)
+        self.id=id
+        self.seed=seed
+        self.creatorid=creatorid
+        self.ownerid=ownerid
+        self.seasonid=seasonid
+        self.centralImageFileName=centralImageFilename
+        self.imageFileName=imageFilename
+        self.promptid=promptid
+
+    def toJson(self):
+        return json.dumps(self.__dict__)
+    def toCardSpecs(self):
+        return super().toJson()
+
+async def checkCardIntegrity(cid:int, hash:str)-> bool:
+    return await check_cardHash(cardid=cid, hash=hash)
