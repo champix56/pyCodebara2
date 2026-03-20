044fa59cbf59de49ae0f91c398035038e76c2232
champix56 <desorbaix@free.fr>
2026-03-20 02:20:26 +0100

    add table for card

--- tmp.sql
+++ tmp.sql
@@ -2,12 +2,38 @@ drop database if exists `cdb`;
 create database `cdb`;
 create user if not exists `codebara`;
 GRANT ALL PRIVILEGES  ON codebara.* TO `codebara`;
-GRANT ALL PRIVILEGES  OuserN *.* TO `champix`;
+GRANT ALL PRIVILEGES  ON *.* TO `champix`;
 use `cdb`;
 
-create table `user`(id bigint auto_increment primary key, nickname varchar(64),amount int, seed int(3) default 1, mail varchar(128));
-create table `card_request`(id bigint auto_increment primary key,userid bigint references user(id), prompt varchar(784 ), request json, daterequest datetime default now(),state int default 0, fileloc varchar(512) );
+CREATE TABLE `user` (
+	`id` BIGINT NOT NULL AUTO_INCREMENT,
+	`nickname` VARCHAR(64) NULL DEFAULT NULL ,
+	`password` CHAR(64) NOT NULL ,
+	`amount` BIGINT NULL DEFAULT NULL,
+	`seed` INT NULL DEFAULT '1',
+	`mail` VARCHAR(128) NULL DEFAULT NULL ,
+	`hash` CHAR(64) AS (sha2(concat(`nickname`,`password`),256)) stored,
+	PRIMARY KEY (`id`)
+);
+CREATE TABLE `card` (
+	`id` BIGINT NOT NULL AUTO_INCREMENT,
+	`creatorid` BIGINT NULL DEFAULT NULL  REFERENCES `user` (`id`) ON UPDATE NO ACTION ON DELETE SET NULL,
+    `ownerid`  BIGINT NULL DEFAULT NULL  REFERENCES `user` (`id`) ON UPDATE NO ACTION ON DELETE SET NULL,
+    `seasonid` INT NOT NULL ,
+	`prompt` VARCHAR(784) NULL DEFAULT NULL ,
+    `seasonPromptid` varchar(16) NULL default NULL,
+	`request` JSON NULL DEFAULT NULL,
+	`daterequest` DATETIME NULL DEFAULT (CURRENT_TIMESTAMP),
+	`state` INT NULL DEFAULT '1',
+	`fileloc` VARCHAR(512) NULL DEFAULT NULL,
+	`cardHash` CHAR(64) NULL DEFAULT NULL,
+	`name` varchar(96) NOT NULL DEFAULT '',
+	`health` INT NOT NULL DEFAULT 0,
+	`attack` INT NOT NULL DEFAULT 0,
+    `cardpower` INTEGER NOT NULL DEFAULT 100,
+	PRIMARY KEY (`id`)
+);
 
 insert into user values (666,'god', 6666666666, 2,'god@codebara.com' );
-INSERT INTO `cdb`.`card_request` (`userid`, `prompt`, `request`, `state`, `fileloc`) VALUES ('666', 'ee', '{"aa":123}', '1', '');
+INSERT INTO `cdb`.`card` (`ownerid`,`creatorid`, `prompt`, `request`, `state`) VALUES ('666','666', 'ee', '{"aa":123}', '1', '');
 
