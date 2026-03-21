772227632221c805505d1de629bb1de7ccb2d5cf
champix56 <desorbaix@free.fr>
2026-03-21 17:07:46 +0100

    add user hash and srv data loc for cards srv save

--- codebara/users/user.py
+++ codebara/users/user.py
@@ -1,13 +1,15 @@
 class UserCoreDatas:
-    def __init__(self, uid: int, seed: int):
+    def __init__(self, uid: int, seed: int,hash :str):
         self.uid = uid
         self.seed = seed
+        self.hash=hash
+        self.serverDataLoc='./contents/user/'+str(uid)
 
 class User(UserCoreDatas):
-    def __init__(self, uid: int, seed: int, nickname: str, mail: str, amount: int):
-        super().__init__(uid, seed)
+    def __init__(self, uid: int, seed: int,hash:str, nickname: str, mail: str, amount: int):
+        super().__init__(uid, seed,hash)
         self.mail = mail
         self.nickname = nickname
         self.amount = amount
     def parentInstance(self)->UserCoreDatas:
-        return UserCoreDatas(self.uid,self.seed)
+        return UserCoreDatas(self.uid,self.seed,self.hash)
