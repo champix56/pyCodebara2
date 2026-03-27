3bf62f9713821507e3d198ccc062df09726415d5
champix56 <desorbaix@free.fr>
2026-03-27 07:32:51 +0100

    update table user

--- tmp.sql
+++ tmp.sql
@@ -5,14 +5,20 @@ GRANT ALL PRIVILEGES  ON codebara.* TO `codebara`;
 GRANT ALL PRIVILEGES  ON *.* TO `champix`;
 use `cdb`;
 
+
+DROP TABLE IF EXISTS user;
+
+DROP TABLE IF EXISTS user;
 CREATE TABLE `user` (
 	`id` BIGINT NOT NULL AUTO_INCREMENT,
-	`nickname` VARCHAR(64) NULL DEFAULT NULL ,
-	`password` CHAR(64) NOT NULL ,
-	`amount` BIGINT NULL DEFAULT NULL,
-	`seed` INT NULL DEFAULT '1',
-	`mail` VARCHAR(128) NULL DEFAULT NULL ,
-	`hash` CHAR(64) AS (sha2(concat(`nickname`,`password`),256)) stored,
+	`nickname` VARCHAR(64) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`password` CHAR(64) NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`amount` BIGINT NULL DEFAULT 0,
+	`seed` INT NOT NULL DEFAULT (lpad(((rand() * 10 )+1)% 6, 1, '0')),
+	`mail` VARCHAR(128) NULL DEFAULT NULL UNIQUE COLLATE  'utf8mb4_0900_ai_ci',
+	`API_request_token` VARCHAR(64) NULL,
+	`API_token` VARCHAR(64)  NULL ,
+	`hash` CHAR(64) NULL,
 	PRIMARY KEY (`id`)
 );
 CREATE TABLE `card` (
@@ -33,7 +39,6 @@ CREATE TABLE `card` (
     `cardpower` INTEGER NOT NULL DEFAULT 100,
 	PRIMARY KEY (`id`)
 );
-
-insert into user values (666,'god', 6666666666, 2,'god@codebara.com' );
+INSERT INTO user(`id`,`nickname`,`password`,`amount`,`mail`,`hash`) values (666,'god',SHA2('god@codebara.comazerty',256), 6666666666, 'god@codebara.com',SHA2('god',256) );
 INSERT INTO `cdb`.`card` (`ownerid`,`creatorid`, `prompt`, `request`, `state`) VALUES ('666','666', 'ee', '{"aa":123}', '1', '');
 
