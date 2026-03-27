65114643b71e8f072027e8da76dad0aa30a51994
champix56 <desorbaix@free.fr>
2026-03-27 04:05:26 +0100

    add change name and reassemble and rechecksum

--- codebara/cards/cardgen.py
+++ codebara/cards/cardgen.py
@@ -14,7 +14,7 @@ import tarfile
 import hashlib
 from aiohttp import web
 from PIL import Image
-from codebara.cards.cardMysql import register_requestCard, update_registered_requestCard
+from codebara.cards.cardMysql import register_requestCard, update_registered_requestCard, checkCardOwner
 from codebara.cards.imageCardCreator import CardImageCreator, CardSpecs, SpecialSpec
 from codebara.config import (
     IMAGE_IA_GEN_BODY_BASE,
@@ -500,3 +500,43 @@ class CardGenerator:
         return (
             web.Response()
         )  # HttpOutputResponse(error=HttpErrors.OK, request=request).toResponse()con
+def changeCardNameInFile(file:str,name:str):
+    with tarfile.open(file) as cardFile:
+        print(cardFile.getnames())
+        fileNameSplit=file.split('/')
+        repfolder='./tmp/untar/'+fileNameSplit[len(fileNameSplit)-1][:-4]
+        if Path(repfolder).exists():
+            shutil.rmtree(repfolder)
+        Path(repfolder).mkdir(parents=True)
+        cardFile.extractall(repfolder)
+        cardFile.close()
+        datas=None
+        with open(repfolder+'/datas.json') as datasJson:
+            datas=json.load(datasJson)
+            datas['name']=name
+            datasJson.close()
+        with open(repfolder+'/datas.json',"w") as fout:
+            json.dump(datas, fout)
+            fout.close()
+        with open(repfolder+'/hashs.json') as datasJson:
+            datas=json.load(datasJson)
+            datas['datas']=getSha256FromFile(repfolder+'/datas.json')
+            datasJson.close()
+        with open(repfolder+'/hashs.json',"w") as fout:
+            json.dump(datas, fout)
+        os.remove(file)
+        with tarfile.open( file, "w:xz") as tar:
+            for name in os.listdir( repfolder ):
+                #if not name.endswith('card.tar.gz'):
+                tar.add(repfolder+'/'+name, arcname=name)
+                os.remove(repfolder+'/'+name)
+        shutil.rmtree(repfolder)
+        tar.close()
+async def setCardName(uid:int,cid:int,name:str)->bool:
+    fileloc=await checkCardOwner(uid,cid)
+    if fileloc is not None:
+        changeCardNameInFile(file=fileloc,name=name)
+        await update_registered_requestCard(cardid=cid,name=name, hash=getSha256FromFile(fileloc))
+        return True
+    else:
+        return False
