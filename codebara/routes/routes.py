a7945ca15d2712b3382fa671f41a19480415e18a
champix56 <desorbaix@free.fr>
2026-03-28 03:04:25 +0100

    add user get route in main route

--- codebara/routes/routes.py
+++ codebara/routes/routes.py
@@ -7,6 +7,9 @@ async def noBodyRoute(request:web.Request,queryArray:tuple)->web.Response:
     match request.path.split('/')[1]:
         case 'card':
             response=await cardRoutes(request=request,queryArray=queryArray)
+        case 'user':
+            response=await userRoutes(request=request,queryArray=queryArray)
+   
     return response
 async def bodyRoute(request:web.Request,body:dict,queryArray:tuple)->web.Response:
     response=assembleHttpRequestError(error=HttpErrors.ERROR_INVALID_ROUTE, request=request).toResponse()
