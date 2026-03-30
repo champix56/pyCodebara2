76db8e9984f3031ad13fa062ef3190e09b59af10
champix56 <desorbaix@free.fr>
2026-03-30 12:50:19 +0200

    export nex functs

--- codebara/cards/__init__.py
+++ codebara/cards/__init__.py
@@ -2,5 +2,13 @@ from .card_route import cardRoutes
 from .cardgen import CardGenerator
 from .imageCardCreator import CardImageCreator
 from .card import CardSpecs,SpecialSpec, Card
+from .cardFunction import setCardName, getCardListOfUser, getSingleCardById
 from .cardMysql import check_cardHash
-__all__=["cardRoutes", "CardGenerator", "CardImageCreator", "SpecialSpec", "CardSpecs","Card", "check_cardHash"]
+__all__=[
+    "cardRoutes",
+    "CardGenerator",
+    "CardImageCreator",
+    "SpecialSpec", "CardSpecs","Card",
+    "setCardName","getCardListOfUser","getSingleCardById",
+    "check_cardHash"
+]
