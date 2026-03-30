f377b0a082aac6cb8e1601d8e587d88047b5a9ea
champix56 <desorbaix@free.fr>
2026-03-30 12:45:56 +0200

    add GET / route

--- codebara/routes/routes.py
+++ codebara/routes/routes.py
@@ -1,5 +1,5 @@
 from aiohttp import web
-from codebara.errors import assembleHttpRequestError, HttpErrors
+from codebara.errors import assembleHttpRequestError, HttpErrors, HttpOutputResponse
 from codebara.cards import  cardRoutes
 from codebara.users import userRoutes
 async def noBodyRoute(request:web.Request,queryArray:tuple)->web.Response:
@@ -9,9 +9,10 @@ async def noBodyRoute(request:web.Request,queryArray:tuple)->web.Response:
             response=await cardRoutes(request=request,queryArray=queryArray)
         case 'user':
             response=await userRoutes(request=request,queryArray=queryArray)
-   
     return response
 async def bodyRoute(request:web.Request,body:dict,queryArray:tuple)->web.Response:
+    if request.path=='/':
+        return HttpOutputResponse(message='Hello on codebara').toResponse()
     response=assembleHttpRequestError(error=HttpErrors.ERROR_INVALID_ROUTE, request=request).toResponse()
     match request.path.split('/')[1]:
         case 'card':
